52c0aa256f3d5d9e4004edb6e96a44d656e147ec
Tom <t.bjerkan@gmail.com>
2022-10-11 18:57:26 +0100

    Shorten header to 'Sign In'

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to continue
+            Sign in
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
