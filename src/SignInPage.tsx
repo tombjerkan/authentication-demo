f6517d4bcc97e0b38724a27dd25ac5780760a59c
Tom <t.bjerkan@gmail.com>
2022-10-12 12:10:05 +0100

    Make inputs transparent when submitting

--- src/SignInPage.tsx
+++ src/SignInPage.tsx
@@ -59,7 +59,10 @@ export default function SignInPage() {
                   disabled={isLoginInProgress}
                   autoComplete="email"
                   required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  className={clsx(
+                    "relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm",
+                    isLoginInProgress && "opacity-50"
+                  )}
                   placeholder="Email address"
                 />
               </div>
@@ -76,7 +79,10 @@ export default function SignInPage() {
                   disabled={isLoginInProgress}
                   autoComplete="current-password"
                   required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  className={clsx(
+                    "relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm",
+                    isLoginInProgress && "opacity-50"
+                  )}
                   placeholder="Password"
                 />
               </div>
