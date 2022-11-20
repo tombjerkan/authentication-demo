5afd6ce60bf83576a630fe6fada3c1bd4fb8d7ee
Tom <t.bjerkan@gmail.com>
2022-11-20 18:31:46 +0000

    Add BodyText stories

---
+++ src/common/components/BodyText.stories.tsx
@@ -0,0 +1,9 @@
+import { faker } from "@faker-js/faker";
+import BodyText from "./BodyText";
+
+export default {
+  title: "BodyText",
+  component: BodyText,
+};
+
+export const Default = () => <BodyText>{faker.lorem.sentences(4)}</BodyText>;
