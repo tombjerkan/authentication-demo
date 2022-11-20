82296a788a0f5d697f3c9a180d707b990240264d
Tom <t.bjerkan@gmail.com>
2022-11-20 19:22:59 +0000

    Add MainHeader stories

---
+++ src/common/components/MainHeader.stories.tsx
@@ -0,0 +1,10 @@
+import MainHeader from "./MainHeader";
+
+export default {
+  title: "MainHeader",
+  component: MainHeader,
+};
+
+export const Default = () => (
+  <MainHeader>Sign up to our application</MainHeader>
+);
