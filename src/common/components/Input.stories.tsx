e3c35c6d5cb04d13fcdba27f2c4d9c592ba45a38
Tom <t.bjerkan@gmail.com>
2022-11-20 19:22:23 +0000

    Add Input stories

---
+++ src/common/components/Input.stories.tsx
@@ -0,0 +1,10 @@
+import Input from "./Input";
+
+export default {
+  title: "Input",
+  component: Input,
+};
+
+export const Default = () => <Input />;
+
+export const Disabled = () => <Input disabled />;
--- src/common/components/Input.tsx
+++ src/common/components/Input.tsx
@@ -6,6 +6,8 @@ type Props = PropsWithoutRef<JSX.IntrinsicElements["input"]>;
 const Input = (props: Props) => (
   <input
     {...props}
+    // Add default type so that styles using [type='text'] are applied even if prop not explicitly set
+    type={props.type ?? "text"}
     className={clsx(
       "relative block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500",
       props.disabled && "opacity-50",
